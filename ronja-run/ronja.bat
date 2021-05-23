b8291a9efb47a64a479640acffb4ee26bfb77cc5
Branislav Beno <branislav.beno@siemens.com>
2021-05-23 10:24:05 +0200

    Add batch script for application start on Windows

---
+++ ronja-run/ronja.bat
@@ -0,0 +1,13 @@
+@echo off
+rem Win
+rem jdeps --print-module-deps --ignore-missing-deps .\ronja-desktop.jar
+rem jlink --verbose --strip-debug --no-man-pages --no-header-files --compress 2 --output runtime --module-path %JAVA_HOME%\jmods --add-modules java.base,java.desktop,java.instrument,java.management.rmi,java.naming,java.prefs,java.scripting,java.security.jgss,java.sql,jdk.httpserver,jdk.jfr,jdk.unsupported
+
+rem Linux
+rem jdeps --print-module-deps --ignore-missing-deps ./ronja-desktop.jar
+rem jlink --verbose --strip-java-debug-attributes --no-man-pages --no-header-files --compress 2 --output runtime --module-path $JAVA_HOME/jmods --add-modules java.base,java.desktop,java.instrument,java.management.rmi,java.naming,java.prefs,java.scripting,java.security.jgss,java.sql,jdk.httpserver,jdk.jfr,jdk.unsupported
+
+set JAVA_HOME=%CD%\runtime
+set PATH=%JAVA_HOME%\bin;%PATH%
+
+start javaw -jar "%CD%\app\ronja-desktop.jar"
