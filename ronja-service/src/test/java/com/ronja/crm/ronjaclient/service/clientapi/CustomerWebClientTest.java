4d4927a9e23d347dd60823d8f9b0d080e4f29a13
Branislav Beno <brano.beno@gmail.com>
2021-08-01 21:36:07 +0200

    Improve tests

--- ronja-service/src/test/java/com/ronja/crm/ronjaclient/service/clientapi/CustomerWebClientTest.java
+++ ronja-service/src/test/java/com/ronja/crm/ronjaclient/service/clientapi/CustomerWebClientTest.java
@@ -19,129 +19,130 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class CustomerWebClientTest {
 
-    private static final String LIST_RESPONSE = """
-            [
-             {
-                "id": 2,
-                "companyName": "EmmaCorp",
-                "category": "LEVEL_2",
-                "focus": "MANUFACTURE",
-                "status": "INACTIVE"
-             },
-             {
-                "id": 1,
-                "companyName": "LeslieCorp",
-                "category": "LEVEL_1",
-                "focus": "MANUFACTURE",
-                "status": "ACTIVE"
-             }
-            ]""";
+  private static final String LIST_RESPONSE = """
+      [
+       {
+          "id": 2,
+          "companyName": "EmmaCorp",
+          "category": "LEVEL_2",
+          "focus": "MANUFACTURE",
+          "status": "INACTIVE"
+       },
+       {
+          "id": 1,
+          "companyName": "LeslieCorp",
+          "category": "LEVEL_1",
+          "focus": "MANUFACTURE",
+          "status": "ACTIVE"
+       }
+      ]""";
 
-    private static final String SINGLE_RESPONSE = """
-            {
-               "id": 0,
-               "companyName": "TestCompany",
-               "category": "LEVEL_1",
-               "focus": "BUILDER",
-               "status": "ACTIVE"
-            }""";
+  private static final String SINGLE_RESPONSE = """
+      {
+         "id": 0,
+         "companyName": "TestCompany",
+         "category": "LEVEL_1",
+         "focus": "BUILDER",
+         "status": "ACTIVE"
+      }""";
 
-    private MockWebServer mockWebServer;
+  private MockWebServer mockWebServer;
 
-    private CustomerWebClient customerWebClient;
+  private CustomerWebClient customerWebClient;
 
-    @BeforeEach
-    public void setUp() throws IOException {
-        this.mockWebServer = new MockWebServer();
-        this.mockWebServer.start();
-        this.customerWebClient = new CustomerWebClient(mockWebServer.url("/").toString());
-    }
+  @BeforeEach
+  public void setUp() throws IOException {
+    this.mockWebServer = new MockWebServer();
+    this.mockWebServer.start();
+    this.customerWebClient = new CustomerWebClient(mockWebServer.url("/").toString());
+  }
 
-    @AfterEach
-    public void shutdown() throws IOException {
-        this.mockWebServer.shutdown();
-    }
+  @AfterEach
+  public void shutdown() throws IOException {
+    this.mockWebServer.shutdown();
+  }
 
-    @Test
-    public void testFetchingCustomerList() {
-        MockResponse mockResponse = new MockResponse()
-                .addHeader("Content-Type", "application/json; charset=utf-8")
-                .setBody(LIST_RESPONSE);
-        this.mockWebServer.enqueue(mockResponse);
+  @Test
+  public void testFetchingCustomerList() {
+    MockResponse mockResponse = new MockResponse()
+        .addHeader("Content-Type", "application/json; charset=utf-8")
+        .setBody(LIST_RESPONSE);
+    this.mockWebServer.enqueue(mockResponse);
 
-        Customer[] customers = customerWebClient.fetchAllCustomers().block();
-        assertAll(() -> {
-            assertThat(customers).isNotNull();
-            assertThat(customers).hasSize(2);
-        });
+    Customer[] customers = customerWebClient.fetchAllCustomers().block();
+    assertAll(() -> {
+      assertThat(customers).isNotNull();
+      assertThat(customers).hasSize(2);
+    });
 
-        Customer customer = customers[0];
-        assertAll(() -> {
-            assertThat(customer.getId()).isEqualTo(2);
-            assertThat(customer.getCompanyName()).isEqualTo("EmmaCorp");
-            assertThat(customer.getCategory()).isEqualTo(Category.LEVEL_2);
-            assertThat(customer.getFocus()).isEqualTo(Focus.MANUFACTURE);
-            assertThat(customer.getStatus()).isEqualTo(Status.INACTIVE);
-        });
-    }
+    Customer customer = customers[0];
+    assertAll(() -> {
+      assertThat(customer.getId()).isEqualTo(2);
+      assertThat(customer.getCompanyName()).isEqualTo("EmmaCorp");
+      assertThat(customer.getCategory()).isEqualTo(Category.LEVEL_2);
+      assertThat(customer.getFocus()).isEqualTo(Focus.MANUFACTURE);
+      assertThat(customer.getStatus()).isEqualTo(Status.INACTIVE);
+    });
+  }
 
-    @Test
-    public void testExceptionsOnFetchingCustomerList() {
-        assertAll(() -> {
-            assertThatThrownBy(() -> {
-                provideResponse(400, "Error occurred.");
-                customerWebClient.fetchAllCustomers().block();
-            }).isInstanceOf(RuntimeException.class);
-            assertThatThrownBy(() -> {
-                provideResponse(500, "Error occurred.");
-                customerWebClient.fetchAllCustomers().block();
-            }).isExactlyInstanceOf(FetchException.class);
-        });
-    }
+  @Test
+  public void testExceptionsOnFetchingCustomerList() {
+    assertAll(() -> {
+      assertThatThrownBy(() -> fetchCustomers(400)).isInstanceOf(RuntimeException.class);
+      assertThatThrownBy(() -> fetchCustomers(500)).isExactlyInstanceOf(FetchException.class);
+    });
+  }
 
-    @Test
-    public void testCreatingNewCustomer() {
-        MockResponse mockResponse = new MockResponse()
-                .addHeader("Content-Type", "application/json; charset=utf-8")
-                .setBody(SINGLE_RESPONSE);
-        this.mockWebServer.enqueue(mockResponse);
+  @Test
+  public void testCreatingNewCustomer() {
+    MockResponse mockResponse = new MockResponse()
+        .addHeader("Content-Type", "application/json; charset=utf-8")
+        .setBody(SINGLE_RESPONSE);
+    this.mockWebServer.enqueue(mockResponse);
 
-        Customer customer = new Customer();
-        customer.setCompanyName("TestCompany");
-        customer.setCategory(Category.LEVEL_1);
-        customer.setFocus(Focus.BUILDER);
-        customer.setStatus(Status.ACTIVE);
-        Customer newCustomer = customerWebClient.createCustomer(customer).block();
+    Customer customer = new Customer();
+    customer.setCompanyName("TestCompany");
+    customer.setCategory(Category.LEVEL_1);
+    customer.setFocus(Focus.BUILDER);
+    customer.setStatus(Status.ACTIVE);
+    Customer newCustomer = customerWebClient.createCustomer(customer).block();
 
-        assertThat(newCustomer).isNotNull();
-        assertThat(newCustomer.toString()).isEqualTo(customer.toString());
-    }
+    assertAll(() -> {
+      assertThat(newCustomer).isNotNull();
+      assertThat(newCustomer).hasToString(customer.toString());
+    });
+  }
 
-    @Test
-    public void testExceptionsOnCreatingNewCustomer() {
-        assertAll(() -> {
-            assertThatThrownBy(() -> propagateExceptionWith500ServerError(
-                    () -> customerWebClient.createCustomer(new Customer()).block()))
-                    .isExactlyInstanceOf(SaveException.class);
-            assertThatThrownBy(() -> propagateExceptionWith400ServerError(
-                    () -> customerWebClient.createCustomer(new Customer()).block()))
-                    .isInstanceOf(RuntimeException.class);
-        });
-    }
+  @Test
+  public void testExceptionsOnCreatingNewCustomer() {
+    assertAll(() -> {
+      assertThatThrownBy(() -> propagateExceptionWith500ServerError(
+          () -> customerWebClient.createCustomer(new Customer()).block()))
+          .isExactlyInstanceOf(SaveException.class);
+      assertThatThrownBy(() -> propagateExceptionWith400ServerError(
+          () -> customerWebClient.createCustomer(new Customer()).block()))
+          .isInstanceOf(RuntimeException.class);
+    });
+  }
 
-    private void propagateExceptionWith400ServerError(Supplier<Customer> supplier) {
-        provideResponse(400, "Error occurred.");
-        supplier.get();
-    }
+  private void propagateExceptionWith400ServerError(Supplier<Customer> supplier) {
+    provideResponse(400, "Error occurred.");
+    supplier.get();
+  }
 
-    private void propagateExceptionWith500ServerError(Supplier<Customer> supplier) {
-        provideResponse(500, "System is down.");
-        supplier.get();
-    }
+  private void propagateExceptionWith500ServerError(Supplier<Customer> supplier) {
+    provideResponse(500, "System is down.");
+    supplier.get();
+  }
 
-    private void provideResponse(int i, String s) {
-        this.mockWebServer.enqueue(new MockResponse()
-                .setResponseCode(i)
-                .setBody(s));
-    }
+  private void fetchCustomers(int i) {
+    provideResponse(i, "Error occurred.");
+    customerWebClient.fetchAllCustomers().block();
+  }
+
+  private void provideResponse(int i, String s) {
+    this.mockWebServer.enqueue(new MockResponse()
+        .setResponseCode(i)
+        .setBody(s));
+  }
 }
