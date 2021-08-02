b46d35701579bde54255e8b57dcb790bac2fe631
Branislav Beno <brano.beno@gmail.com>
2021-08-02 23:20:16 +0200

    Add test for customer deleting

--- ronja-service/src/main/java/com/ronja/crm/ronjaclient/service/clientapi/CustomerWebClient.java
+++ ronja-service/src/main/java/com/ronja/crm/ronjaclient/service/clientapi/CustomerWebClient.java
@@ -4,61 +4,82 @@ import com.ronja.crm.ronjaclient.service.domain.Customer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 public class CustomerWebClient {
 
-    private final WebClient webClient;
+  public static final String SERVER_ERROR_OCCURRED = "Server error occurred";
+  private final WebClient webClient;
 
-    public CustomerWebClient(String baseUrl) {
-        this.webClient = WebClient.builder()
-                .baseUrl(baseUrl)
-                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-                .build();
-    }
+  public CustomerWebClient(String baseUrl) {
+    this.webClient = WebClient.builder()
+        .baseUrl(baseUrl)
+        .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+        .build();
+  }
 
-    public Mono<Customer[]> fetchAllCustomers() {
-        return webClient.get()
-                .uri("/list")
-                .retrieve()
-                .onStatus(HttpStatus::is4xxClientError, response -> response.bodyToMono(Throwable.class))
-                .onStatus(HttpStatus::is5xxServerError, response -> {
-                    Mono<String> errorMsg = response.bodyToMono(String.class);
-                    return errorMsg.flatMap(msg -> {
-                        throw new FetchException(msg);
-                    });
-                })
-                .bodyToMono(Customer[].class);
-    }
+  public Mono<Customer[]> fetchAllCustomers() {
+    return webClient.get()
+        .uri("/list")
+        .retrieve()
+        .onStatus(HttpStatus::is4xxClientError, this::handleClientError)
+        .onStatus(HttpStatus::is5xxServerError, this::handleFetchingError)
+        .bodyToMono(Customer[].class);
+  }
 
-    public Mono<Customer> createCustomer(Customer customer) {
-        return webClient.post()
-                .uri("/add")
-                .body(Mono.just(customer), Customer.class)
-                .retrieve()
-                .onStatus(HttpStatus::is4xxClientError, response -> response.bodyToMono(Throwable.class))
-                .onStatus(HttpStatus::is5xxServerError, response -> {
-                    Mono<String> errorMsg = response.bodyToMono(String.class);
-                    return errorMsg.flatMap(msg -> {
-                        throw new SaveException(msg);
-                    });
-                })
-                .bodyToMono(Customer.class);
-    }
+  public Mono<Customer> createCustomer(Customer customer) {
+    return webClient.post()
+        .uri("/add")
+        .body(Mono.just(customer), Customer.class)
+        .retrieve()
+        .onStatus(HttpStatus::is4xxClientError, this::handleClientError)
+        .onStatus(HttpStatus::is5xxServerError, this::handleSavingError)
+        .bodyToMono(Customer.class);
+  }
 
-    public Mono<Customer> updateCustomer(Customer customer) {
-        return webClient.post()
-                .uri("/update")
-                .body(Mono.just(customer), Customer.class)
-                .retrieve()
-                .bodyToMono(Customer.class);
-    }
+  public Mono<Customer> updateCustomer(Customer customer) {
+    return webClient.post()
+        .uri("/update")
+        .body(Mono.just(customer), Customer.class)
+        .retrieve()
+        .onStatus(HttpStatus::is4xxClientError, this::handleClientError)
+        .onStatus(HttpStatus::is5xxServerError, this::handleSavingError)
+        .bodyToMono(Customer.class);
+  }
 
-    public Mono<Void> deleteCustomer(int id) {
-        return webClient.delete()
-                .uri("/delete/" + id)
-                .retrieve()
-                .bodyToMono(Void.class);
-    }
+  public Mono<Void> deleteCustomer(int id) {
+    return webClient.delete()
+        .uri("/delete/" + id)
+        .retrieve()
+        .onStatus(HttpStatus::is4xxClientError, this::handleClientError)
+        .onStatus(HttpStatus::is5xxServerError, this::handleDeletingError)
+        .bodyToMono(Void.class);
+  }
+
+  private Mono<Throwable> handleClientError(ClientResponse response) {
+    return response.bodyToMono(Throwable.class);
+  }
+
+  private Mono<Throwable> handleDeletingError(ClientResponse response) {
+    Mono<String> errorMsg = response.bodyToMono(String.class);
+    return errorMsg.flatMap(msg -> {
+      throw new DeleteException(SERVER_ERROR_OCCURRED);
+    });
+  }
+
+  private Mono<Throwable> handleSavingError(ClientResponse response) {
+    Mono<String> errorMsg = response.bodyToMono(String.class);
+    return errorMsg.flatMap(msg -> {
+      throw new SaveException(SERVER_ERROR_OCCURRED);
+    });
+  }
+
+  private Mono<Throwable> handleFetchingError(ClientResponse response) {
+    Mono<String> errorMsg = response.bodyToMono(String.class);
+    return errorMsg.flatMap(msg -> {
+      throw new FetchException(SERVER_ERROR_OCCURRED);
+    });
+  }
 }
--- ronja-service/src/test/java/com/ronja/crm/ronjaclient/service/clientapi/CustomerWebClientTest.java
+++ ronja-service/src/test/java/com/ronja/crm/ronjaclient/service/clientapi/CustomerWebClientTest.java
@@ -14,151 +14,176 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.function.Supplier;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class CustomerWebClientTest {
 
-    private static final String LIST_RESPONSE = """
-            [
-             {
-                "id": 2,
-                "companyName": "EmmaCorp",
-                "category": "LEVEL_2",
-                "focus": "MANUFACTURE",
-                "status": "INACTIVE"
-             },
-             {
-                "id": 1,
-                "companyName": "LeslieCorp",
-                "category": "LEVEL_1",
-                "focus": "MANUFACTURE",
-                "status": "ACTIVE"
-             }
-            ]""";
+  private static final String LIST_RESPONSE = """
+      [
+       {
+          "id": 2,
+          "companyName": "EmmaCorp",
+          "category": "LEVEL_2",
+          "focus": "MANUFACTURE",
+          "status": "INACTIVE"
+       },
+       {
+          "id": 1,
+          "companyName": "LeslieCorp",
+          "category": "LEVEL_1",
+          "focus": "MANUFACTURE",
+          "status": "ACTIVE"
+       }
+      ]""";
 
-    private static final String SINGLE_RESPONSE = """
-            {
-               "id": 0,
-               "companyName": "TestCompany",
-               "category": "LEVEL_1",
-               "focus": "BUILDER",
-               "status": "ACTIVE"
-            }""";
+  private static final String SINGLE_RESPONSE = """
+      {
+         "id": 0,
+         "companyName": "TestCompany",
+         "category": "LEVEL_1",
+         "focus": "BUILDER",
+         "status": "ACTIVE"
+      }""";
 
-    private MockWebServer mockWebServer;
+  private MockWebServer mockWebServer;
 
-    private CustomerWebClient customerWebClient;
+  private CustomerWebClient customerWebClient;
 
-    @BeforeEach
-    public void setUp() throws IOException {
-        this.mockWebServer = new MockWebServer();
-        this.mockWebServer.start();
-        this.customerWebClient = new CustomerWebClient(mockWebServer.url("/").toString());
-    }
+  @BeforeEach
+  public void setUp() throws IOException {
+    this.mockWebServer = new MockWebServer();
+    this.mockWebServer.start();
+    this.customerWebClient = new CustomerWebClient(mockWebServer.url("/").toString());
+  }
 
-    @AfterEach
-    public void shutdown() throws IOException {
-        this.mockWebServer.shutdown();
-    }
+  @AfterEach
+  public void shutdown() throws IOException {
+    this.mockWebServer.shutdown();
+  }
 
-    @Test
-    @DisplayName("Customer list fetching: happy path test")
-    public void testCustomerListFetching() {
-        mockResponse(LIST_RESPONSE);
+  @Test
+  @DisplayName("Customer list fetching: happy path test")
+  public void testCustomerListFetching() {
+    mockResponse(LIST_RESPONSE);
 
-        Customer[] customers = customerWebClient.fetchAllCustomers().block();
-        assertAll(() -> {
-            assertThat(customers).isNotNull();
-            assertThat(customers).hasSize(2);
-        });
+    Customer[] customers = customerWebClient.fetchAllCustomers().block();
+    assertAll(() -> {
+      assertThat(customers).isNotNull();
+      assertThat(customers).hasSize(2);
+    });
 
-        Customer customer = customers[0];
-        assertAll(() -> {
-            assertThat(customer.getId()).isEqualTo(2);
-            assertThat(customer.getCompanyName()).isEqualTo("EmmaCorp");
-            assertThat(customer.getCategory()).isEqualTo(Category.LEVEL_2);
-            assertThat(customer.getFocus()).isEqualTo(Focus.MANUFACTURE);
-            assertThat(customer.getStatus()).isEqualTo(Status.INACTIVE);
-        });
-    }
+    Customer customer = customers[0];
+    assertAll(() -> {
+      assertThat(customer.getId()).isEqualTo(2);
+      assertThat(customer.getCompanyName()).isEqualTo("EmmaCorp");
+      assertThat(customer.getCategory()).isEqualTo(Category.LEVEL_2);
+      assertThat(customer.getFocus()).isEqualTo(Focus.MANUFACTURE);
+      assertThat(customer.getStatus()).isEqualTo(Status.INACTIVE);
+    });
+  }
 
-    @Test
-    @DisplayName("Customer creating: happy path test")
-    public void testCustomerCreating() {
-        mockResponse(SINGLE_RESPONSE);
+  @Test
+  @DisplayName("Customer creating: happy path test")
+  public void testCustomerCreating() {
+    mockResponse(SINGLE_RESPONSE);
 
-        Customer customer = provideCustomer();
-        Customer newCustomer = customerWebClient.createCustomer(customer).block();
+    Customer customer = provideCustomer();
+    Customer newCustomer = customerWebClient.createCustomer(customer).block();
 
-        assertAll(() -> {
-            assertThat(newCustomer).isNotNull();
-            assertThat(newCustomer).hasToString(customer.toString());
-        });
-    }
+    assertAll(() -> {
+      assertThat(newCustomer).isNotNull();
+      assertThat(newCustomer).hasToString(customer.toString());
+    });
+  }
 
-    @Test
-    @DisplayName("Customer updating: happy path test")
-    public void testCustomerUpdating() {
-        mockResponse(SINGLE_RESPONSE);
-        Customer updatedCustomer = customerWebClient.updateCustomer(new Customer()).block();
-        assertThat(updatedCustomer).isNotNull();
-    }
+  @Test
+  @DisplayName("Customer updating: happy path test")
+  public void testCustomerUpdating() {
+    mockResponse(SINGLE_RESPONSE);
+    Customer updatedCustomer = customerWebClient.updateCustomer(new Customer()).block();
+    assertThat(updatedCustomer).isNotNull();
+  }
 
-    @Test
-    @DisplayName("Customer data handling: failure test")
-    public void testExceptionsOnCustomerDataHandling() {
-        assertAll(() -> {
-            // customer list fetching failures
-            assertThatThrownBy(() -> fetchCustomers(400)).isInstanceOf(RuntimeException.class);
-            assertThatThrownBy(() -> fetchCustomers(500)).isExactlyInstanceOf(FetchException.class);
-            // customer creating failures
-            assertThatThrownBy(() -> propagateExceptionWith500ServerError(
-                    () -> customerWebClient.createCustomer(new Customer()).block()))
-                    .isExactlyInstanceOf(SaveException.class);
-            assertThatThrownBy(() -> propagateExceptionWith400ServerError(
-                    () -> customerWebClient.createCustomer(new Customer()).block()))
-                    .isInstanceOf(RuntimeException.class);
-            // ToDo: customer updating failures
-            // ToDo: customer deleting failures
-        });
-    }
+  @Test
+  @DisplayName("Customer deleting: happy path test")
+  public void testCustomerDeleting() {
+    mockResponse();
+    assertThatCode(() -> customerWebClient.deleteCustomer(0).block()).doesNotThrowAnyException();
+  }
 
-    private void propagateExceptionWith400ServerError(Supplier<Customer> supplier) {
-        provideResponse(400, "Error occurred.");
-        supplier.get();
-    }
+  @Test
+  @DisplayName("Customer data handling: failure test")
+  public void testExceptionsOnCustomerDataHandling() {
+    assertAll(() -> {
+      // customer list fetching failures
+      assertThatThrownBy(() -> fetchCustomers(400)).isInstanceOf(RuntimeException.class);
+      assertThatThrownBy(() -> fetchCustomers(500)).isExactlyInstanceOf(FetchException.class);
+      // customer creating failures
+      assertThatThrownBy(() -> propagateExceptionWith500ServerError(
+          () -> customerWebClient.createCustomer(new Customer()).block()))
+          .isExactlyInstanceOf(SaveException.class);
+      assertThatThrownBy(() -> propagateExceptionWith400ServerError(
+          () -> customerWebClient.createCustomer(new Customer()).block()))
+          .isInstanceOf(RuntimeException.class);
+      // customer updating failures
+      assertThatThrownBy(() -> propagateExceptionWith500ServerError(
+          () -> customerWebClient.updateCustomer(new Customer()).block()))
+          .isExactlyInstanceOf(SaveException.class);
+      assertThatThrownBy(() -> propagateExceptionWith400ServerError(
+          () -> customerWebClient.updateCustomer(new Customer()).block()))
+          .isInstanceOf(RuntimeException.class);
+      // customer deleting failures
+      assertThatThrownBy(() -> deleteCustomer(400)).isInstanceOf(RuntimeException.class);
+      assertThatThrownBy(() -> deleteCustomer(500)).isExactlyInstanceOf(DeleteException.class);
+    });
+  }
 
-    private void propagateExceptionWith500ServerError(Supplier<Customer> supplier) {
-        provideResponse(500, "System is down.");
-        supplier.get();
-    }
+  private void propagateExceptionWith400ServerError(Supplier<Customer> supplier) {
+    provideResponse(400, "Error occurred.");
+    supplier.get();
+  }
 
-    private void fetchCustomers(int i) {
-        provideResponse(i, "Error occurred.");
-        customerWebClient.fetchAllCustomers().block();
-    }
+  private void propagateExceptionWith500ServerError(Supplier<Customer> supplier) {
+    provideResponse(500, "System is down.");
+    supplier.get();
+  }
 
-    private void provideResponse(int i, String s) {
-        this.mockWebServer.enqueue(new MockResponse()
-                .setResponseCode(i)
-                .setBody(s));
-    }
+  private void fetchCustomers(int i) {
+    provideResponse(i, "Error occurred.");
+    customerWebClient.fetchAllCustomers().block();
+  }
 
-    private void mockResponse(String listResponse) {
-        MockResponse mockResponse = new MockResponse()
-                .addHeader("Content-Type", "application/json; charset=utf-8")
-                .setBody(listResponse);
-        this.mockWebServer.enqueue(mockResponse);
-    }
+  private void deleteCustomer(int i) {
+    provideResponse(i, "Error occurred.");
+    customerWebClient.deleteCustomer(0).block();
+  }
 
-    private Customer provideCustomer() {
-        Customer customer = new Customer();
-        customer.setCompanyName("TestCompany");
-        customer.setCategory(Category.LEVEL_1);
-        customer.setFocus(Focus.BUILDER);
-        customer.setStatus(Status.ACTIVE);
-        return customer;
-    }
+  private void provideResponse(int i, String s) {
+    this.mockWebServer.enqueue(new MockResponse()
+        .setResponseCode(i)
+        .setBody(s));
+  }
+
+  private void mockResponse(String listResponse) {
+    MockResponse mockResponse = new MockResponse()
+        .addHeader("Content-Type", "application/json; charset=utf-8")
+        .setBody(listResponse);
+    this.mockWebServer.enqueue(mockResponse);
+  }
+
+  private void mockResponse() {
+    MockResponse mockResponse = new MockResponse()
+        .addHeader("Content-Type", "application/json; charset=utf-8");
+    this.mockWebServer.enqueue(mockResponse);
+  }
+
+  private Customer provideCustomer() {
+    Customer customer = new Customer();
+    customer.setCompanyName("TestCompany");
+    customer.setCategory(Category.LEVEL_1);
+    customer.setFocus(Focus.BUILDER);
+    customer.setStatus(Status.ACTIVE);
+    return customer;
+  }
 }
