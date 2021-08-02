865630a1f4f4986ae1a422e0cde18406d847a462
Branislav Beno <brano.beno@gmail.com>
2021-08-02 23:21:22 +0200

    Add test Exception class for deleting

---
+++ ronja-service/src/main/java/com/ronja/crm/ronjaclient/service/clientapi/DeleteException.java
@@ -0,0 +1,8 @@
+package com.ronja.crm.ronjaclient.service.clientapi;
+
+public class DeleteException extends RuntimeException {
+
+  public DeleteException(String message) {
+    super(message);
+  }
+}
