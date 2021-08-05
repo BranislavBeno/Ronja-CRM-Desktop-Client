f360b5a00bb007d65da02bde12359fcc0d308362
Branislav Beno <brano.beno@gmail.com>
2021-08-06 00:02:59 +0200

    Get rid of obsolete search bar

--- ronja-desktop/src/main/java/com/ronja/crm/ronjaclient/desktop/component/util/TableViewUtil.java
+++ ronja-desktop/src/main/java/com/ronja/crm/ronjaclient/desktop/component/util/TableViewUtil.java
@@ -2,7 +2,6 @@ package com.ronja.crm.ronjaclient.desktop.component.util;
 
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Pos;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import org.controlsfx.control.tableview2.FilteredTableColumn;
 import org.controlsfx.control.tableview2.filter.filtereditor.SouthFilter;
@@ -11,26 +10,26 @@ import java.util.function.Function;
 
 public class TableViewUtil {
 
-    private TableViewUtil() {
-    }
+  private TableViewUtil() {
+  }
 
-    public static <T, U> TableColumn<T, U> addColumn(String name, TableView<T> table, Class<U> clazz,
-                                                     Function<T, ObservableValue<U>> function) {
-        return addColumn(name, Pos.CENTER, table, clazz, function);
-    }
+  public static <T, U> FilteredTableColumn<T, U> addColumn(String name, TableView<T> table, Class<U> clazz,
+                                                           Function<T, ObservableValue<U>> function) {
+    return addColumn(name, Pos.CENTER, table, clazz, function);
+  }
 
-    public static <T, U> FilteredTableColumn<T, U> addColumn(String caption, Pos alignment, TableView<T> table,
-                                                             Class<U> clazz, Function<T, ObservableValue<U>> function) {
-        FilteredTableColumn<T, U> column = new FilteredTableColumn<>(caption);
-        column.setCellValueFactory(p -> function.apply(p.getValue()));
-        column.setStyle(String.format("-fx-alignment: %s;", alignment));
+  public static <T, U> FilteredTableColumn<T, U> addColumn(String caption, Pos alignment, TableView<T> table,
+                                                           Class<U> clazz, Function<T, ObservableValue<U>> function) {
+    FilteredTableColumn<T, U> column = new FilteredTableColumn<>(caption);
+    column.setCellValueFactory(p -> function.apply(p.getValue()));
+    column.setStyle(String.format("-fx-alignment: %s;", alignment));
 
-        SouthFilter<T, U> filter = new SouthFilter<>(column, clazz);
-        column.setSouthNode(filter);
+    SouthFilter<T, U> filter = new SouthFilter<>(column, clazz);
+    column.setSouthNode(filter);
 
-        table.getColumns().add(column);
+    table.getColumns().add(column);
 
-        return column;
-    }
+    return column;
+  }
 
 }
