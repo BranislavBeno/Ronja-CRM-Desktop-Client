cd431aea3b717dd0856986c9612031b3ebf2610e
Branislav Beno <brano.beno@gmail.com>
2021-08-18 08:07:31 +0200

    Extend representative table with single value attributes

--- ronja-desktop/src/main/java/com/ronja/crm/ronjaclient/desktop/component/representative/RepresentativeTableItem.java
+++ ronja-desktop/src/main/java/com/ronja/crm/ronjaclient/desktop/component/representative/RepresentativeTableItem.java
@@ -1,5 +1,6 @@
 package com.ronja.crm.ronjaclient.desktop.component.representative;
 
+import com.ronja.crm.ronjaclient.service.domain.Customer;
 import com.ronja.crm.ronjaclient.service.domain.Representative;
 import com.ronja.crm.ronjaclient.service.domain.Status;
 import javafx.beans.property.ReadOnlyObjectProperty;
@@ -7,108 +8,155 @@ import javafx.beans.property.ReadOnlyStringProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 
+import java.time.LocalDate;
 import java.util.Objects;
 
 public class RepresentativeTableItem {
 
-  private final SimpleStringProperty firstName;
-  private final SimpleStringProperty lastName;
-  private final SimpleStringProperty position;
-  private final SimpleStringProperty region;
-  private final SimpleStringProperty notice;
-  private final SimpleObjectProperty<Status> status;
-  private final Representative representative;
+    private final SimpleStringProperty firstName;
+    private final SimpleStringProperty lastName;
+    private final SimpleStringProperty position;
+    private final SimpleStringProperty region;
+    private final SimpleObjectProperty<Customer> customer;
+    private final SimpleStringProperty notice;
+    private final SimpleObjectProperty<Status> status;
+    private final SimpleObjectProperty<LocalDate> lastVisit;
+    private final SimpleObjectProperty<LocalDate> scheduledVisit;
 
-  public RepresentativeTableItem(Representative representative) {
-    this.representative = Objects.requireNonNull(representative, "Je potrebné zadať reprezentanta!");
+    private final Representative representative;
 
-    firstName = new SimpleStringProperty(representative.getFirstName());
-    lastName = new SimpleStringProperty(representative.getLastName());
-    position = new SimpleStringProperty(representative.getPosition());
-    region = new SimpleStringProperty(representative.getRegion());
-    notice = new SimpleStringProperty(representative.getNotice());
-    status = new SimpleObjectProperty<>(representative.getStatus());
-  }
+    public RepresentativeTableItem(Representative representative) {
+        this.representative = Objects.requireNonNull(representative, "Je potrebné zadať reprezentanta!");
 
-  public String getFirstName() {
-    return firstName.get();
-  }
+        firstName = new SimpleStringProperty(representative.getFirstName());
+        lastName = new SimpleStringProperty(representative.getLastName());
+        position = new SimpleStringProperty(representative.getPosition());
+        region = new SimpleStringProperty(representative.getRegion());
+        customer = new SimpleObjectProperty<>(representative.getCustomer());
+        notice = new SimpleStringProperty(representative.getNotice());
+        status = new SimpleObjectProperty<>(representative.getStatus());
+        lastVisit = new SimpleObjectProperty<>(representative.getLastVisit());
+        scheduledVisit = new SimpleObjectProperty<>(representative.getScheduledVisit());
+    }
 
-  public ReadOnlyStringProperty firstNameProperty() {
-    return firstName;
-  }
+    public String getFirstName() {
+        return firstName.get();
+    }
 
-  public void setFirstName(String firstName) {
-    this.firstName.set(firstName);
-    this.representative.setFirstName(firstName);
-  }
+    public ReadOnlyStringProperty firstNameProperty() {
+        return firstName;
+    }
 
-  public String getLastName() {
-    return lastName.get();
-  }
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+        this.representative.setFirstName(firstName);
+    }
 
-  public ReadOnlyStringProperty lastNameProperty() {
-    return lastName;
-  }
+    public String getLastName() {
+        return lastName.get();
+    }
 
-  public void setLastName(String lastName) {
-    this.lastName.set(lastName);
-    this.representative.setLastName(lastName);
-  }
+    public ReadOnlyStringProperty lastNameProperty() {
+        return lastName;
+    }
 
-  public String getPosition() {
-    return position.get();
-  }
+    public void setLastName(String lastName) {
+        this.lastName.set(lastName);
+        this.representative.setLastName(lastName);
+    }
 
-  public ReadOnlyStringProperty positionProperty() {
-    return position;
-  }
+    public String getPosition() {
+        return position.get();
+    }
 
-  public void setPosition(String position) {
-    this.position.set(position);
-    this.representative.setPosition(position);
-  }
+    public ReadOnlyStringProperty positionProperty() {
+        return position;
+    }
 
-  public String getRegion() {
-    return region.get();
-  }
+    public void setPosition(String position) {
+        this.position.set(position);
+        this.representative.setPosition(position);
+    }
 
-  public ReadOnlyStringProperty regionProperty() {
-    return region;
-  }
+    public String getRegion() {
+        return region.get();
+    }
 
-  public void setRegion(String region) {
-    this.region.set(region);
-    this.representative.setRegion(region);
-  }
+    public ReadOnlyStringProperty regionProperty() {
+        return region;
+    }
 
-  public String getNotice() {
-    return notice.get();
-  }
+    public void setRegion(String region) {
+        this.region.set(region);
+        this.representative.setRegion(region);
+    }
 
-  public ReadOnlyStringProperty noticeProperty() {
-    return notice;
-  }
+    public Customer getCustomer() {
+        return customer.get();
+    }
 
-  public void setNotice(String notice) {
-    this.notice.set(notice);
-    this.representative.setNotice(notice);
-  }
+    public ReadOnlyStringProperty customerProperty() {
+        return new SimpleStringProperty(customer.get().getCompanyName());
+    }
 
-  public Status getStatus() {
-    return status.get();
-  }
+    public void setCustomer(Customer customer) {
+        this.customer.set(customer);
+        this.representative.setCustomer(customer);
+    }
 
-  public ReadOnlyObjectProperty<Status> statusProperty() {
-    return status;
-  }
+    public String getNotice() {
+        return notice.get();
+    }
 
-  public void setStatus(Status status) {
-    this.status.set(status);
-    this.representative.setStatus(status);
-  }
+    public ReadOnlyStringProperty noticeProperty() {
+        return notice;
+    }
 
-  public Representative getRepresentative() {
-    return representative;
-  }
+    public void setNotice(String notice) {
+        this.notice.set(notice);
+        this.representative.setNotice(notice);
+    }
+
+    public Status getStatus() {
+        return status.get();
+    }
+
+    public ReadOnlyObjectProperty<Status> statusProperty() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status.set(status);
+        this.representative.setStatus(status);
+    }
+
+    public Representative getRepresentative() {
+        return representative;
+    }
+
+    public LocalDate getLastVisit() {
+        return lastVisit.get();
+    }
+
+    public ReadOnlyObjectProperty<LocalDate> lastVisitProperty() {
+        return lastVisit;
+    }
+
+    public void setLastVisit(LocalDate lastVisit) {
+        this.lastVisit.set(lastVisit);
+        this.representative.setLastVisit(lastVisit);
+    }
+
+    public LocalDate getScheduledVisit() {
+        return scheduledVisit.get();
+    }
+
+    public ReadOnlyObjectProperty<LocalDate> scheduledVisitProperty() {
+        return scheduledVisit;
+    }
+
+    public void setScheduledVisit(LocalDate scheduledVisit) {
+        this.scheduledVisit.set(scheduledVisit);
+        this.representative.setScheduledVisit(scheduledVisit);
+    }
 }
--- ronja-desktop/src/main/java/com/ronja/crm/ronjaclient/desktop/component/representative/RepresentativeTableView.java
+++ ronja-desktop/src/main/java/com/ronja/crm/ronjaclient/desktop/component/representative/RepresentativeTableView.java
@@ -4,6 +4,7 @@ import com.ronja.crm.ronjaclient.desktop.component.common.FetchException;
 import com.ronja.crm.ronjaclient.desktop.component.util.TableViewUtil;
 import com.ronja.crm.ronjaclient.service.clientapi.RepresentativeWebClient;
 import com.ronja.crm.ronjaclient.service.domain.Representative;
+import com.ronja.crm.ronjaclient.service.domain.Status;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -15,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Objects;
@@ -23,62 +25,63 @@ import java.util.stream.Stream;
 @Component
 public class RepresentativeTableView extends VBox {
 
-  @Value("${client.representatives.base-url}")
-  String baseUrl;
-  @Autowired
-  private final RepresentativeWebClient representativeWebClient;
+    @Value("${client.representatives.base-url}")
+    String baseUrl;
+    @Autowired
+    private final RepresentativeWebClient representativeWebClient;
 
-  private final ObservableList<RepresentativeTableItem> tableItems;
-  private final FilteredTableView<RepresentativeTableItem> tableView;
+    private final ObservableList<RepresentativeTableItem> tableItems;
+    private final FilteredTableView<RepresentativeTableItem> tableView;
 
-  public RepresentativeTableView(RepresentativeWebClient representativeWebClient) {
-    this.representativeWebClient = Objects.requireNonNull(representativeWebClient);
+    public RepresentativeTableView(RepresentativeWebClient representativeWebClient) {
+        this.representativeWebClient = Objects.requireNonNull(representativeWebClient);
 
-    tableView = new FilteredTableView<>();
-    getChildren().add(tableView);
+        tableView = new FilteredTableView<>();
+        getChildren().add(tableView);
 
-    tableItems = FXCollections.observableArrayList();
-    addItems();
-    setUpTableView();
-    FilteredTableView.configureForFiltering(tableView, tableItems);
-  }
-
-  public void refreshItems() {
-    tableItems.clear();
-    addItems();
-  }
-
-  private void addItems() {
-    Platform.runLater(() -> fetchRepresentatives().forEach(this::addItem));
-  }
-
-  private Stream<Representative> fetchRepresentatives() {
-    try {
-      Representative[] representatives = Objects.requireNonNull(representativeWebClient.fetchAllRepresentatives().block());
-      return Arrays.stream(representatives).sorted(Comparator.comparing(Representative::getLastName));
-    } catch (Exception e) {
-      throw new FetchException("""
-          Nepodarilo sa získať dáta o reprezentantoch.
-          Preverte spojenie so serverom.""", e);
+        tableItems = FXCollections.observableArrayList();
+        addItems();
+        setUpTableView();
+        FilteredTableView.configureForFiltering(tableView, tableItems);
     }
-  }
 
-  private void addItem(Representative representative) {
-    var item = new RepresentativeTableItem(representative);
-    addItem(item);
-  }
+    public void refreshItems() {
+        tableItems.clear();
+        addItems();
+    }
 
-  public void addItem(RepresentativeTableItem item) {
-    tableItems.add(item);
-  }
+    private void addItems() {
+        Platform.runLater(() -> fetchRepresentatives().forEach(this::addItem));
+    }
 
-  private void setUpTableView() {
-    TableViewUtil.addColumn("Meno",
-        Pos.CENTER_LEFT, tableView, String.class, RepresentativeTableItem::firstNameProperty);
-    TableViewUtil.addColumn("Priezvisko", tableView, String.class, RepresentativeTableItem::lastNameProperty);
-    TableViewUtil.addColumn("Pozícia", tableView, String.class, RepresentativeTableItem::positionProperty);
-    TableViewUtil.addColumn("Región", tableView, String.class, RepresentativeTableItem::regionProperty);
+    private Stream<Representative> fetchRepresentatives() {
+        try {
+            Representative[] representatives = Objects.requireNonNull(representativeWebClient.fetchAllRepresentatives().block());
+            return Arrays.stream(representatives).sorted(Comparator.comparing(Representative::getLastName));
+        } catch (Exception e) {
+            throw new FetchException("""
+                    Nepodarilo sa získať dáta o reprezentantoch.
+                    Preverte spojenie so serverom.""", e);
+        }
+    }
 
-    VBox.setVgrow(tableView, Priority.ALWAYS);
-  }
+    private void addItem(Representative representative) {
+        var item = new RepresentativeTableItem(representative);
+        tableItems.add(item);
+    }
+
+    private void setUpTableView() {
+        TableViewUtil.addColumn("Meno",
+                Pos.CENTER_LEFT, tableView, String.class, RepresentativeTableItem::firstNameProperty);
+        TableViewUtil.addColumn("Priezvisko", tableView, String.class, RepresentativeTableItem::lastNameProperty);
+        TableViewUtil.addColumn("Pozícia", tableView, String.class, RepresentativeTableItem::positionProperty);
+        TableViewUtil.addColumn("Región", tableView, String.class, RepresentativeTableItem::regionProperty);
+        TableViewUtil.addColumn("Spoločnosť", tableView, String.class, RepresentativeTableItem::customerProperty);
+        TableViewUtil.addColumn("Poznámka", tableView, String.class, RepresentativeTableItem::noticeProperty);
+        TableViewUtil.addColumn("Stav", tableView, Status.class, RepresentativeTableItem::statusProperty);
+        TableViewUtil.addColumn("Posledné stretnutie", tableView, LocalDate.class, RepresentativeTableItem::lastVisitProperty);
+        TableViewUtil.addColumn("Plánované stretnutie", tableView, LocalDate.class, RepresentativeTableItem::scheduledVisitProperty);
+
+        VBox.setVgrow(tableView, Priority.ALWAYS);
+    }
 }
