edf14efc3f30592654beb1de9281a838adb9dd54
Branislav Beno <branislav.beno@siemens.com>
2021-08-25 19:24:43 +0200

    Method Customer.toString must not return null

--- ronja-service/src/main/java/com/ronja/crm/ronjaclient/service/domain/Customer.java
+++ ronja-service/src/main/java/com/ronja/crm/ronjaclient/service/domain/Customer.java
@@ -56,7 +56,7 @@ public class Customer {
 
     @Override
     public String toString() {
-        return companyName;
+        return companyName != null ? companyName : "";
     }
 
     @Override
