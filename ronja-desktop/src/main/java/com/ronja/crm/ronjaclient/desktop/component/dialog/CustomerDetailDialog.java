1756c7c770a173476c6aff0915cbbdfd44f25663
Branislav Beno <branislav.beno@siemens.com>
2021-08-27 08:44:36 +0200

    Fix method for updating existing customer

--- ronja-desktop/src/main/java/com/ronja/crm/ronjaclient/desktop/component/dialog/CustomerDetailDialog.java
+++ ronja-desktop/src/main/java/com/ronja/crm/ronjaclient/desktop/component/dialog/CustomerDetailDialog.java
@@ -26,151 +26,151 @@ import java.util.concurrent.CompletableFuture;
 
 public final class CustomerDetailDialog extends Stage {
 
-  private final CustomerWebClient webClient;
-  private final CustomerTableView tableView;
-  private final TextField companyNameTextField;
-  private final ChoiceBox<Category> categoryChoiceBox;
-  private final ChoiceBox<Focus> focusChoiceBox;
-  private final ChoiceBox<Status> statusChoiceBox;
-  private final Button saveButton;
+    private final CustomerWebClient webClient;
+    private final CustomerTableView tableView;
+    private final TextField companyNameTextField;
+    private final ChoiceBox<Category> categoryChoiceBox;
+    private final ChoiceBox<Focus> focusChoiceBox;
+    private final ChoiceBox<Status> statusChoiceBox;
+    private final Button saveButton;
 
-  public CustomerDetailDialog(CustomerWebClient webClient,
-                              CustomerTableView tableView,
-                              boolean update) {
-    this.webClient = Objects.requireNonNull(webClient);
-    this.tableView = Objects.requireNonNull(tableView);
+    public CustomerDetailDialog(CustomerWebClient webClient,
+                                CustomerTableView tableView,
+                                boolean update) {
+        this.webClient = Objects.requireNonNull(webClient);
+        this.tableView = Objects.requireNonNull(tableView);
 
-    initOwner(App.getMainWindow());
-    initModality(Modality.WINDOW_MODAL);
-    setResizable(false);
+        initOwner(App.getMainWindow());
+        initModality(Modality.WINDOW_MODAL);
+        setResizable(false);
 
-    companyNameTextField = new TextField();
-    categoryChoiceBox = new ChoiceBox<>();
-    categoryChoiceBox.setItems(FXCollections.observableArrayList(Category.values()));
-    focusChoiceBox = new ChoiceBox<>();
-    focusChoiceBox.setItems(FXCollections.observableArrayList(Focus.values()));
-    statusChoiceBox = new ChoiceBox<>();
-    statusChoiceBox.setItems(FXCollections.observableArrayList(Status.values()));
-    saveButton = new Button();
+        companyNameTextField = new TextField();
+        categoryChoiceBox = new ChoiceBox<>();
+        categoryChoiceBox.setItems(FXCollections.observableArrayList(Category.values()));
+        focusChoiceBox = new ChoiceBox<>();
+        focusChoiceBox.setItems(FXCollections.observableArrayList(Focus.values()));
+        statusChoiceBox = new ChoiceBox<>();
+        statusChoiceBox.setItems(FXCollections.observableArrayList(Status.values()));
+        saveButton = new Button();
 
-    initialize(update);
-  }
-
-  private void initialize(boolean update) {
-    if (update) {
-      setUpDialogForUpdate();
-    } else {
-      setUpDialogForCreate();
+        initialize(update);
     }
 
-    var cancelButton = new Button("Zruš");
-    cancelButton.setOnAction(e -> DesktopUtil.cancelOperation(getScene()));
+    private void initialize(boolean update) {
+        if (update) {
+            setUpDialogForUpdate();
+        } else {
+            setUpDialogForCreate();
+        }
 
-    var buttonBar = new HBox();
-    buttonBar.setAlignment(Pos.CENTER_RIGHT);
-    buttonBar.setSpacing(10);
-    buttonBar.getChildren().addAll(cancelButton, saveButton);
-    var detailViewPane = setUpGridPane();
-    var vBox = new VBox();
-    vBox.getChildren().addAll(detailViewPane, buttonBar);
-    vBox.setPadding(new Insets(12, 10, 12, 10));
-    vBox.setSpacing(10);
+        var cancelButton = new Button("Zruš");
+        cancelButton.setOnAction(e -> DesktopUtil.cancelOperation(getScene()));
 
-    var scene = new Scene(vBox, 400, 170);
-    setScene(scene);
-  }
+        var buttonBar = new HBox();
+        buttonBar.setAlignment(Pos.CENTER_RIGHT);
+        buttonBar.setSpacing(10);
+        buttonBar.getChildren().addAll(cancelButton, saveButton);
+        var detailViewPane = setUpGridPane();
+        var vBox = new VBox();
+        vBox.getChildren().addAll(detailViewPane, buttonBar);
+        vBox.setPadding(new Insets(12, 10, 12, 10));
+        vBox.setSpacing(10);
 
-  private void setUpDialogForUpdate() {
-    Customer customer = tableView.selectedCustomer().getValue().getCustomer();
-    setUpContent(customer);
-    setTitle("Upraviť zákazníka");
-    saveButton.setText("Ulož");
-    updateCustomer(customer);
-    saveButton.setOnAction(e -> {
-      CompletableFuture
-          .runAsync(() -> webClient.updateCustomer(customer).block())
-          .whenComplete((r, t) -> updateCustomerItem(t));
-      DesktopUtil.cancelOperation(getScene());
-    });
-  }
-
-  private void setUpDialogForCreate() {
-    setUpContent();
-    setTitle("Pridať zákazníka");
-    saveButton.setText("Pridaj");
-    saveButton.setOnAction(e -> {
-      Customer customer = provideCustomer();
-      CompletableFuture
-          .runAsync(() -> webClient.createCustomer(customer).block())
-          .whenComplete((r, t) -> addCustomerItem(customer, t));
-      DesktopUtil.cancelOperation(getScene());
-    });
-  }
-
-  private void updateCustomerItem(Throwable throwable) {
-    if (throwable == null) {
-      CustomerTableItem item = tableView.selectedCustomer().getValue();
-      item.setCompanyName(companyNameTextField.getText());
-      item.setCategory(categoryChoiceBox.getValue());
-      item.setFocus(focusChoiceBox.getValue());
-      item.setStatus(statusChoiceBox.getValue());
+        var scene = new Scene(vBox, 400, 170);
+        setScene(scene);
     }
-  }
 
-  private void addCustomerItem(Customer customer, Throwable throwable) {
-    if (throwable == null) {
-      CustomerTableItem item = new CustomerTableItem(customer);
-      tableView.addItem(item);
+    private void setUpDialogForUpdate() {
+        Customer customer = tableView.selectedCustomer().getValue().getCustomer();
+        setUpContent(customer);
+        setTitle("Upraviť zákazníka");
+        saveButton.setText("Ulož");
+        saveButton.setOnAction(e -> {
+            Customer updatedCustomer = updateCustomer(customer);
+            CompletableFuture
+                    .runAsync(() -> webClient.updateCustomer(updatedCustomer).block())
+                    .whenComplete((r, t) -> updateCustomerItem(t));
+            DesktopUtil.cancelOperation(getScene());
+        });
     }
-  }
 
-  private Customer provideCustomer() {
-    var customer = new Customer();
-    updateCustomer(customer);
-    return customer;
-  }
+    private void setUpDialogForCreate() {
+        setUpContent();
+        setTitle("Pridať zákazníka");
+        saveButton.setText("Pridaj");
+        saveButton.setOnAction(e -> {
+            Customer customer = provideCustomer();
+            CompletableFuture
+                    .runAsync(() -> webClient.createCustomer(customer).block())
+                    .whenComplete((r, t) -> addCustomerItem(customer, t));
+            DesktopUtil.cancelOperation(getScene());
+        });
+    }
 
-  private void updateCustomer(Customer customer) {
-    customer.setCompanyName(companyNameTextField.getText());
-    customer.setCategory(categoryChoiceBox.getValue());
-    customer.setFocus(focusChoiceBox.getValue());
-    customer.setStatus(statusChoiceBox.getValue());
-  }
+    private void updateCustomerItem(Throwable throwable) {
+        if (throwable == null) {
+            CustomerTableItem item = tableView.selectedCustomer().getValue();
+            item.setCompanyName(companyNameTextField.getText());
+            item.setCategory(categoryChoiceBox.getValue());
+            item.setFocus(focusChoiceBox.getValue());
+            item.setStatus(statusChoiceBox.getValue());
+        }
+    }
 
-  private GridPane setUpGridPane() {
-    Label companyNameLabel = new Label("Názov spoločnosti:");
-    Label categoryLabel = new Label("Kategória:");
-    Label focusLabel = new Label("Zameranie:");
-    Label statusLabel = new Label("Stav:");
+    private void addCustomerItem(Customer customer, Throwable throwable) {
+        if (throwable == null) {
+            CustomerTableItem item = new CustomerTableItem(customer);
+            tableView.addItem(item);
+        }
+    }
 
-    var gridPane = new GridPane();
-    gridPane.addRow(0, companyNameLabel, companyNameTextField);
-    gridPane.addRow(1, categoryLabel, categoryChoiceBox);
-    gridPane.addRow(2, focusLabel, focusChoiceBox);
-    gridPane.addRow(3, statusLabel, statusChoiceBox);
+    private Customer provideCustomer() {
+        var customer = new Customer();
+        return updateCustomer(customer);
+    }
 
-    gridPane.setAlignment(Pos.CENTER_LEFT);
-    gridPane.setHgap(5);
-    gridPane.setVgap(5);
-    var columnConstraints = new ColumnConstraints();
-    columnConstraints.setHgrow(Priority.ALWAYS);
-    gridPane.getColumnConstraints().addAll(new ColumnConstraints(), columnConstraints);
-    VBox.setVgrow(gridPane, Priority.NEVER);
+    private Customer updateCustomer(Customer customer) {
+        customer.setCompanyName(companyNameTextField.getText());
+        customer.setCategory(categoryChoiceBox.getValue());
+        customer.setFocus(focusChoiceBox.getValue());
+        customer.setStatus(statusChoiceBox.getValue());
+        return customer;
+    }
 
-    return gridPane;
-  }
+    private GridPane setUpGridPane() {
+        Label companyNameLabel = new Label("Názov spoločnosti:");
+        Label categoryLabel = new Label("Kategória:");
+        Label focusLabel = new Label("Zameranie:");
+        Label statusLabel = new Label("Stav:");
 
-  private void setUpContent(Customer customer) {
-    companyNameTextField.setText(customer.getCompanyName());
-    categoryChoiceBox.setValue(customer.getCategory());
-    focusChoiceBox.setValue(customer.getFocus());
-    statusChoiceBox.setValue(customer.getStatus());
-  }
+        var gridPane = new GridPane();
+        gridPane.addRow(0, companyNameLabel, companyNameTextField);
+        gridPane.addRow(1, categoryLabel, categoryChoiceBox);
+        gridPane.addRow(2, focusLabel, focusChoiceBox);
+        gridPane.addRow(3, statusLabel, statusChoiceBox);
 
-  private void setUpContent() {
-    companyNameTextField.setText("");
-    categoryChoiceBox.setValue(Category.LEVEL_1);
-    focusChoiceBox.setValue(Focus.BUILDER);
-    statusChoiceBox.setValue(Status.ACTIVE);
-  }
+        gridPane.setAlignment(Pos.CENTER_LEFT);
+        gridPane.setHgap(5);
+        gridPane.setVgap(5);
+        var columnConstraints = new ColumnConstraints();
+        columnConstraints.setHgrow(Priority.ALWAYS);
+        gridPane.getColumnConstraints().addAll(new ColumnConstraints(), columnConstraints);
+        VBox.setVgrow(gridPane, Priority.NEVER);
+
+        return gridPane;
+    }
+
+    private void setUpContent(Customer customer) {
+        companyNameTextField.setText(customer.getCompanyName());
+        categoryChoiceBox.setValue(customer.getCategory());
+        focusChoiceBox.setValue(customer.getFocus());
+        statusChoiceBox.setValue(customer.getStatus());
+    }
+
+    private void setUpContent() {
+        companyNameTextField.setText("");
+        categoryChoiceBox.setValue(Category.LEVEL_1);
+        focusChoiceBox.setValue(Focus.BUILDER);
+        statusChoiceBox.setValue(Status.ACTIVE);
+    }
 }
