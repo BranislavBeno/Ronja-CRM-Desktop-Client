0cb209d33d7801f5c3278a78ac3b157194f6edb5
Branislav Beno <branislav.beno@siemens.com>
2021-11-09 13:28:30 +0100

    Simplify windows batch script

--- ronja-run/ronja.bat
+++ ronja-run/ronja.bat
@@ -1,9 +1,6 @@
 @echo off
 
 rem jdeps --print-module-deps --ignore-missing-deps .\ronja-desktop.jar
-rem jlink --verbose --strip-debug --no-man-pages --no-header-files --compress 2 --output runtime --module-path %JAVA_HOME%\jmods --add-modules java.base,java.desktop,java.instrument,java.management.rmi,java.naming,java.prefs,java.scripting,java.security.jgss,java.sql,jdk.httpserver,jdk.jfr,jdk.unsupported
+rem jlink --verbose --strip-debug --no-man-pages --no-header-files --compress 2 --output runtime --module-path %JAVA_HOME%\jmods --add-modules java.base,java.desktop,java.instrument,java.management,java.naming,java.prefs,java.rmi,java.scripting,java.sql,jdk.httpserver,jdk.jfr,jdk.unsupported
 
-set JAVA_HOME=%CD%\runtime
-set PATH=%JAVA_HOME%\bin;%PATH%
-echo %JAVA_HOME%
-start javaw -jar "%CD%\app\ronja-desktop.jar"
+start %CD%\runtime\bin\java -jar app\ronja-desktop.jar
